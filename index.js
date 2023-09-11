c89048f31df69b99db3421bb9ba1cfd7c38fdc3c
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2023-09-11 16:12:46 -0400

    Create button for creating div elements
    Included a button that calls a prompt for the user to input the desired
    dimension for the square grid. Used console.log(typeof dimension) to see
    if the value entered is a string or number. Since it was a string, I
    used parseInt(dimension) to convert the string into a number to later
    generate the desired grid of div elements.

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 // Create a 16 x 16 grid of div elements inside "grid-container"
 let element;
 
+const screen = document.querySelector('body');
 const container = document.querySelector('.grid-container');
+const pixelButton =document.getElementById('pixels');
 
+container.remove();
 
-function createDiv() {
-    for (let i = 1; i <= 256; i++) {
-        element = "div" + i;
-        console.log(element);
-        element = document.createElement('div');
-        element.className = 'small-div';
-        container.appendChild(element);
+pixelButton.addEventListener('click', () => {
+    function pressButton() {
+        ask = prompt("Type the dimension you want the square grid to be:", "0");
+        return ask;
     }
-    return element;
-}
-createDiv();
+    const dimension = pressButton();
+    const numberDimension = parseInt(dimension);
+    console.log(numberDimension);
+    console.log(typeof numberDimension);
+    
+    console.log("This is the dimension: " + numberDimension + " x " + numberDimension);
+    
+})
+  
+
+/*
+
+
+
+    function createContainer() {
+        screen.appendChild(container);
+        for (let i = 1; i <= 256; i++) {
+            element = "div" + i;
+            //console.log(element);
+            element = document.createElement('div');
+            element.className = 'small-div';
+            container.appendChild(element);
+        }
+        return element;
+    }
+    createContainer();
 
 const divs = document.querySelectorAll('.small-div');
 
@@ -26,3 +49,4 @@ divs.forEach((div) => {
             this.style.backgroundColor = 'yellowgreen';
     });
 });
+ */
