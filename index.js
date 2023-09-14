a7b83c19d2853db77b5a6afad457e89d609b33ab
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2023-09-14 14:15:42 -0400

    Fix color-changing error
    To get the div elements to change color when the mouse moved over them,
    I had to move the 'mouseenter' and 'mouseleave' event listeners inside
    the click event handler for the button. I changed the CSS display
    setting to grid, removed the j variable and changed the for loop
    condition to "i < dimension * dimension". Inside the button's click
    event handler, I included "container.innerHTML = '';" to remove the grid
    whenever the button is clicked.

--- index.js
+++ index.js
@@ -4,36 +4,30 @@ let element;
 const screen = document.querySelector('body');
 const container = document.querySelector('.grid-container');
 const pixelButton =document.getElementById('pixels');
-const divs = document.querySelectorAll('.small-div');
 
 pixelButton.addEventListener('click', () => {
-    function askPrompt() {
-        ask = prompt("How many squares do you want in each column and row?", "0");
-        return ask;
-    }
-    askPrompt();
+    container.innerHTML = '';
 
-    const dimension = parseInt(askPrompt());
-    console.log(dimension);
-    console.log(typeof dimension);
+    const ask = prompt("How many squares do you want in each column and row?", "0");
+    const dimension = parseInt(ask);
+
     console.log("This is the grid's dimensions: " + dimension + " x " + dimension);
 
     function createSketchPad() {
         screen.appendChild(container);
-        for (let i = 0; i < dimension; i++) {
-            for (let j = 0; j < dimension; j++) {
-                container.style.border = "1px solid black";
-                container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
-                container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
-                element = document.createElement('div');
-                element.className = 'small-div';
-                container.appendChild(element);
-            }    
-        }
-        return element;
+        container.style.border = "1px solid black";
+        container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+
+        for (let i = 0; i < dimension * dimension; i++) {
+            element = document.createElement('div');
+            element.classList.add('small-div');
+        container.appendChild(element);
+        }   
     }
     createSketchPad();
-    
+
+    const divs = document.querySelectorAll('.small-div');
     divs.forEach((div) => {
         div.addEventListener('mouseenter', function () {
                 this.style.backgroundColor = '#34fc34';
@@ -42,7 +36,9 @@ pixelButton.addEventListener('click', () => {
                 this.style.backgroundColor = '#34fc34';
         });
     });
+    
 })
+
 
 
 
