907888a59784f20eb81f605c6af7f698193136bc
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2023-09-14 10:08:07 -0400

    Attempt to create button to make new grid
    Wanted to fix the formatting issues with the grid container and div
    elements when a new grid was created. Tried to create a new button that
    would erase the old grid before making a new grid. The original button
    became disabled. Could not recreate grid or fix the formatting issues.

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ let element;
 const screen = document.querySelector('body');
 const container = document.querySelector('.grid-container');
 const pixelButton =document.getElementById('pixels');
-
-container.remove();
+const divs = document.querySelectorAll('.small-div');
 
 pixelButton.addEventListener('click', () => {
-    function pressButton() {
+    function askPrompt() {
         ask = prompt("How many squares do you want in each column and row?", "0");
         return ask;
     }
-    const dimension = pressButton();
-    const numberDimension = parseInt(dimension);
-    console.log(numberDimension);
-    console.log(typeof numberDimension);
-    
-    console.log("This is the grid's dimensions: " + numberDimension + " x " + numberDimension);
-    
-    function createContainer() {
+    askPrompt();
+
+    const dimension = parseInt(askPrompt());
+    console.log(dimension);
+    console.log(typeof dimension);
+    console.log("This is the grid's dimensions: " + dimension + " x " + dimension);
+
+    function createSketchPad() {
         screen.appendChild(container);
         for (let i = 0; i < dimension; i++) {
             for (let j = 0; j < dimension; j++) {
-                container.style.gridTemplateRows = `repeat(${numberDimension}, 1fr)`;
-                container.style.gridTemplateColumns = `repeat(${numberDimension}, 1fr)`;
+                container.style.border = "1px solid black";
+                container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+                container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
                 element = document.createElement('div');
                 element.className = 'small-div';
                 container.appendChild(element);
@@ -32,10 +32,8 @@ pixelButton.addEventListener('click', () => {
         }
         return element;
     }
-    createContainer();
-
-    const divs = document.querySelectorAll('.small-div');
-
+    createSketchPad();
+    
     divs.forEach((div) => {
         div.addEventListener('mouseenter', function () {
                 this.style.backgroundColor = '#34fc34';
@@ -45,6 +43,13 @@ pixelButton.addEventListener('click', () => {
         });
     });
 })
+
+
+
+
+
+
+
 
    
 
